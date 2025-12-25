22a39ab01495643d4d653b30ee971d07c9a69a12
mochnasir1111-del <mochnasir1111@gmail.com>
2025-12-25 20:45:19 +0700

    Delete assets/js/ads-floating.js

--- assets/js/ads-floating.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const ad = document.getElementById("floatingAd");
-  const closeBtn = document.getElementById("closeAd");
-
-  // tombol close
-  closeBtn.addEventListener("click", function () {
-    ad.style.display = "none";
-  });
-});
